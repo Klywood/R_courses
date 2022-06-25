7f216780b5719c415063f17468a931cddbae123d
Andrew <gish.gish@inbox.ru>
2022-06-25 11:42:00 +0300

    study R

---
+++ Analysis of nominative data.R
@@ -0,0 +1,99 @@
+setwd("C:/Users/User/Desktop/proj/R_lang")
+
+
+df <-  read.csv('grants.csv')
+
+str(df)
+
+#  creating factor
+?factor
+df$status <- factor(df$status, labels = c('Not funded', 'Funded'))
+
+# table 1
+t1 <- table(df$status)
+t1
+dim(t1)
+
+# table 2
+t2 <- table(status = df$status, field = df$field)
+t2
+dim(t2)
+
+
+prop.table(t2) #  summary
+prop.table(t2, 1) #  by rows
+prop.table(t2, 2) #  by cols
+
+# table 3
+t3 <- table(Years = df$years_in_uni, Field = df$field, Status = df$status)
+t3
+dim(t3)
+
+
+#  task 1
+#  Ваша задача в переменную red_men сохранить долю рыжеволосых (Red) 
+#    от общего числа голубоглазых мужчин.
+
+red_men <- prop.table(HairEyeColor[ , ,'Male'], 2)
+red_men['Red', 'Blue']
+
+#  task 2
+#  Напишите число зеленоглазых женщин в наборе данных HairEyeColor
+sum(HairEyeColor[ , ,'Female'][, 'Green'])
+
+
+#  Plots
+barplot(t2)
+barplot(t2, legend.text = T, args.legend = list(x = 'topright'))
+barplot(t2, legend.text = T, args.legend = list(x = 'topright'), beside = T)
+
+mosaicplot(t2)
+
+#  task 3
+#  Постройте столбчатую диаграмму распределения цвета глаз по цвету волос только у женщин
+library(psych)
+library(ggplot2)
+mydata <- as.data.frame(HairEyeColor)
+?geom_bar
+women  <-  subset(x = mydata, subset = mydata$Sex == 'Female')
+ggplot(data = women, aes(x = Hair, y = Freq, fill = Eye)) + 
+  geom_bar(stat="identity", position = "dodge") + 
+  scale_fill_manual(values=c("Brown", "Blue", "Darkgrey", "Darkgreen"))
+
+#  Binomial Test
+binom.test(x = 5, n = 20, p = 0.5)
+binom.test(t1)
+
+#  Chi-Square Test
+chisq.test(t1)
+chisq.test(t2)
+
+#  Fisher's Exact Test
+fisher.test(t2)
+
+#  task 4
+# https://stepik.org/lesson/11502/step/12?unit=2525
+w_brown <- HairEyeColor['Brown', ,'Female']
+chisq.test(w_brown)
+
+#  task 5
+#  https://stepik.org/lesson/11502/step/13?unit=2525
+diamonds
+main_stat <- chisq.test(table(Cut = diamonds$cut, Color = diamonds$color))$statistic
+
+#  task 6
+#  https://stepik.org/lesson/11502/step/14?unit=2525
+diamonds
+my_diam <- diamonds
+mean_price <- mean(my_diam$price)
+mean_carat <- mean(my_diam$carat)
+
+my_diam$factor_price <- ifelse(test = my_diam$price >= mean_price, 1, 0)
+my_diam$factor_carat <- ifelse(test = my_diam$carat >= mean_carat, 1, 0)
+
+main_stat <- chisq.test(table(my_diam$factor_price, my_diam$factor_carat))$statistic
+
+#  task 7
+#  https://stepik.org/lesson/11502/step/15?unit=2525
+fisher_test <- fisher.test(table(mtcars$am, mtcars$vs))$p.value
+
---
+++ statistics_plots.R
@@ -0,0 +1,108 @@
+setwd("C:/Users/User/Desktop/proj/R_lang")
+
+?mtcars
+
+
+df <- mtcars
+
+str(df)
+
+df$vs <- factor(df$vs, labels = c("V", "S"))
+df$am <- factor(df$am, labels = c("Auto", "Manual"))
+
+library(psych)
+
+?describe
+
+#  Main statistics
+
+descr = describe(x = df[, -c(8,9)])
+
+
+descr2 <-  describeBy(x = df[, -c(8,9)], group = df$vs, mat = T, digits = 1)
+
+
+descr3 <-  describeBy(x = df[, -c(8,9)], group = df$vs, mat = T, digits = 1, fast = T)
+
+
+describeBy(df$qsec, list(df$vs, df$am), digits = 1,
+           fast = T)
+
+
+
+df$mpg[1:10] <- NA
+
+
+mean(df$mpg, na.rm = T)
+
+data <- airquality
+
+new <- subset(data, Month %in% c(7,8,9))
+
+result <- aggregate(Ozone ~ Month, new, length)
+
+str(airquality)
+
+describeBy(airquality, list(airquality$Month),
+           digits = 1)
+
+describeBy(iris, fast = T)
+
+
+aggregate(x=iris, by=list(iris$Species), FUN=median, )
+
+describeBy(iris, group = list(iris$Species))
+
+describeBy(iris, group = iris$Species)$'virginica'['median']
+
+my_vector <- rnorm(30)
+
+my_vector[sample(1:30, 10)] <- NA
+is.na(my_vector)
+
+replace(my_vector, is.na(my_vector), mean(my_vector, na.rm = T))
+
+# Simple plots
+
+hist(df$mpg, breaks = 20, xlab = 'MPG')
+
+boxplot(mpg ~ am, df, ylab = 'MPG')
+
+plot(df$mpg, df$hp)
+
+#  ggplot2
+
+library(ggplot2)
+
+ggplot(df, aes(x = mpg, fill = am)) + 
+  geom_dotplot()
+
+ggplot(df, aes(x = mpg, fill = am)) + 
+  geom_density(alpha=0.4)
+
+
+ggplot(df, aes(x = am, y = hp, col = vs)) +
+  geom_boxplot()
+
+ggplot(df, aes(x = mpg, y = hp, col = vs, size = qsec)) +
+  geom_point(alpha = 0.4)
+
+
+ggplot(data, aes(x = as.factor(Month), y = Ozone)) +
+  geom_boxplot(na.rm = T)
+
+plot1 <- ggplot(mtcars, aes(x = mpg, y = disp, col = hp))+
+  geom_point()
+
+
+plot2 <- ggplot(iris, aes(Sepal.Length, Sepal.Width, col = Species, size = Petal.Length)) +
+  geom_point()
+
+#  saving data
+write.csv(df, 'mtcars.csv')
+#  saving variables
+save(descr, file='descr.RData')
+
+
+
+
